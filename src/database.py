27e26ae8b074c75af5251532bc2f0f6379307d2a
Christopher Tant <tant@vir-aureum.local>
2022-10-14 08:45:14 -0400

    Completed module

--- src/database.py
+++ src/database.py
@@ -1,4 +1,6 @@
 import csv
+from src.reminder import PoliteReminder
+
 
 def list_reminders():
     f = open("reminders.csv", "r")
@@ -15,5 +17,6 @@ def list_reminders():
 def add_reminder(text):
 
     with open('reminders.csv', 'a+', newline='\n') as file:
+        reminder = PoliteReminder(text)
         writer = csv.writer(file)
-        writer.writerow([text])
+        writer.writerow([reminder.text])
